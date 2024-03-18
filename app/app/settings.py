1b7c7a1fe423f9fbbe2457dd1073e4cc0545a59c
Mario <maruchito2003@gmail.com>
2024-03-18 11:12:00 -0500

    avance

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'academics.apps.AcademicsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +79,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'app',
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+       'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),  
     }
 }
 
